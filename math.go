bd5ab7d42944320c1a7fd6febab5cb381a35a1ea
Luiz Alves Rodrigues <luiz.rodrigues@tjdft.jus.br>
2025-01-12 21:37:53 -0300

    Soma a soma a divisao

--- math.go
+++ math.go
@@ -3,9 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(soma(112, 10))
+	fmt.Println(soma(112, 10) + divide(110, 5))
 }
 
 func soma(a int, b int) int {
 	return a + b
+}
+
+func divide(a int, b int) int {
+	return a / b
 }
